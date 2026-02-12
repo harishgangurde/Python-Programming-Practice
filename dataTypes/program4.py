4aeebb124210a3abcdba6ed0fbed491712356a95
Harish Gangurde <harishgangurde24@gmail.com>
2026-02-12 10:08:51 +0530

    DataTypes

---
+++ dataTypes/program4.py
@@ -0,0 +1,15 @@
+
+dataList = ["Ashish","Kanha","Rahul","Madhur","Kanha"]
+
+print(dataList)
+
+print(dataList[1])      #Kanha
+print(dataList[3])      #Madhur
+#print(dataList[-6])     #RangeError
+
+dataList[1] = "Rahul"
+
+print(dataList)
+
+print(type(dataList))   #<class'List'>
+
