644a08e241d8adbdfe71db24a738caff68ba831d
Harish Gangurde <harishgangurde24@gmail.com>
2026-02-13 20:17:20 +0530

    DataTypes

---
+++ dataTypes/program6.py
@@ -0,0 +1,14 @@
+
+#Dictionary
+
+player = {18:"Virat",7:"MSD",45:"Rohit"}
+
+print(player)
+print(type(player))
+
+print(player[7])  #MSD
+
+player[7] = "Mahi"
+print(player)
+
+print(player[-2]) #error
