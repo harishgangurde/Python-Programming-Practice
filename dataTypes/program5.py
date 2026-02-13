9bea1a4c651a793877e22b3a7c83dadbf1cc46b3
Harish Gangurde <harishgangurde24@gmail.com>
2026-02-13 20:16:55 +0530

    DataTypes

---
+++ dataTypes/program5.py
@@ -0,0 +1,13 @@
+
+dataTuple = [10,"Kanha",20.5,"Madhur"]
+
+print(dataTuple)
+
+print(dataTuple[1])      #Kanha
+print(dataTuple[2])      #20.5
+print(dataTuple[-4])     #10
+
+print(type(dataTuple))   #<class'tuple'>
+
+dataTuple[1] = "Rahul"      #Error
+
