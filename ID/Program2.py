ca47d319cb7c50f939eee18052214bd6b5dc0529
Harish Gangurde <harishgangurde24@gmail.com>
2026-02-20 21:05:20 +0530

    ID

---
+++ ID/Program2.py
@@ -0,0 +1,17 @@
+
+empID = 10
+empName = 'Kanha'
+
+print(empID)
+print(empName)
+
+print(id(empID))
+print(id(empName))
+
+empID = 20
+
+print(empID)
+print(empName)
+
+print(id(empID))
+print(id(empName))
