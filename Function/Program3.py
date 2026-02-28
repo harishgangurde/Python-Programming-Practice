6fea8c0135bc49130d3316537c6cb704e1001915
Harish Gangurde <harishgangurde24@gmail.com>
2026-02-28 22:58:10 +0530

    Function

---
+++ Function/Program3.py
@@ -0,0 +1,10 @@
+#Positional argument
+
+def empData(empName,empID):
+    print("Employee name: ",empName)
+    print("Emplpyee ID: ",empID)
+
+print("Start code")
+empData("Rohan",48)
+empData("Akash",1)
+print("End code")
