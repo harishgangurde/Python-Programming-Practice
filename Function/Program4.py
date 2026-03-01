60fc0bb42d8e371751b8cb7a9a665eea8a3a0e68
Harish Gangurde <harishgangurde24@gmail.com>
2026-03-01 14:57:52 +0530

    Function

---
+++ Function/Program4.py
@@ -0,0 +1,10 @@
+#keyword argument
+
+def empData(empName,empID):
+    print("Employee name: ",empName)
+    print("Emplpyee ID: ",empID)
+
+print("Start code")
+empData("Rohan",48)
+empData(empID = 1,empName = "Akash")
+print("End code")
