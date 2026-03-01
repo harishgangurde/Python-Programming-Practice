b6610c275dff4add85537d8216bbf1b29f6392bb
Harish Gangurde <harishgangurde24@gmail.com>
2026-03-01 14:58:38 +0530

    Function

---
+++ Function/Program5.py
@@ -0,0 +1,11 @@
+
+#Default Argument
+
+def playerData(playerName,jerNo=18,country="India"):
+    print(playerName)
+    print(jerNo)
+    print(country)
+
+playerData("Virat")
+playerData("Rohit",45)
+playerData("Warner",31,"Aus")
