d0f656436c9c340f86cfba50ea9c8ee7f61ce3bd
Harish Gangurde <harishgangurde24@gmail.com>
2026-03-08 20:52:31 +0530

    Control Statements

---
+++ controlStatement/program5.py
@@ -0,0 +1,27 @@
+
+
+Driver = input("Enter F1 Driver Name : ")
+
+if Driver == "Max" or Driver == "Yuki" :
+    print("Red Bull")
+
+elif Driver == "Charles" or Driver == "Lewis" :
+    print("Ferrari")
+
+elif Driver == "George" or Driver == "Kimi" :
+    print("Mercedes")
+
+elif Driver == "Liam" or Driver == "Hadjar" :
+    print("Racing Bulls")
+
+elif Driver == "Fernando" or Driver == "Lance" :
+    print("Aston Martin")
+
+elif Driver == "Lando" or Driver == "Oscar" :
+    print("Mclaren")
+
+else :
+    print("Not an F1 Driver")
+
+
+
