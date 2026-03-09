309a70a2c35ce6669e4cd833d30aeaf01e9df2be
Harish Gangurde <harishgangurde24@gmail.com>
2026-03-09 21:39:12 +0530

    Control Statements

---
+++ controlStatement/program7.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
+
+if num1 > num2 and num1 > num3 :
+    print(num1,"is greater")
+
+elif num2 > num3 :
+    print(num2,"is greater")
+
+else :
+    print(num3,"is greater")
