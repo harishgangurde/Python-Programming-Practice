f3eec582b6ab0cd3c47a4331a9b1c9aa866e3b7f
Harish Gangurde <harishgangurde24@gmail.com>
2026-03-28 22:31:22 +0530

    Inheritance

---
+++ Inheritance/AccessModifier/program2.py
@@ -0,0 +1,11 @@
+
+class Demo:
+    def __init__(self):
+        print("In Constructor")
+        self.x = 10
+        self.y = 20
+
+class Memo:
+    def __init__(self):
+        print("In Constructor Memo")
+
